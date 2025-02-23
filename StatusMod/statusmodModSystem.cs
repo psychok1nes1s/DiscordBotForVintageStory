d7a3500708a569c407a1668cea642978084f9895
psychok1nes1s <rus4ynuk@mail.ru>
2025-02-23 22:20:11 +0300

    Initial commit: объединение C# мода и Discord бота

---
+++ StatusMod/statusmodModSystem.cs
@@ -0,0 +1,12 @@
+﻿using Vintagestory.API.Common;
+
+[assembly: ModInfo("statusmod", "statusmod",
+                    Authors = new string[] { "Unknown" },
+                    Description = "This is a sample mod",
+                    Version = "1.0.0")]
+namespace statusmod
+{
+    public class statusmodModSystem : ModSystem
+    {
+    }
+}
