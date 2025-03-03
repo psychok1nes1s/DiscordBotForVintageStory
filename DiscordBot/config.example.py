66d6889addd8ab0d00e99dc274f76a7e3abba8e6
psychok1nes1s <rus4ynuk@mail.ru>
2025-03-04 02:10:57 +0300

    Release 1.1.0

--- DiscordBot/config.example.py
+++ DiscordBot/config.example.py
@@ -1,10 +1,10 @@
 class Config:
-    DISCORD_TOKEN = 'YOUR_DISCORD_BOT_TOKEN'  # Замените на ваш токен Discord
+    DISCORD_TOKEN = 'YOUR_DISCORD_BOT_TOKEN'  # Замените на ваш токен
     VS_SERVER_URL = 'http://localhost:8080/status/'  # URL вашего сервера (с завершающим слешем)
-    REQUEST_TIMEOUT = 30  # Таймаут запроса в секундах
+    REQUEST_TIMEOUT = 30  # Значительно увеличенный таймаут запроса в секундах
     
     # ID канала Discord для отправки уведомлений о шторме
-    NOTIFICATION_CHANNEL_ID = "YOUR_CHANNEL_ID"  # Замените на ID вашего канала Discord
+    NOTIFICATION_CHANNEL_ID = "YOUR_CHANNEL_ID"  # Замените на ID вашего канала  
     # Порт для HTTP сервера, который будет принимать уведомления от игрового сервера
     NOTIFICATION_PORT = 8081 
-    SERVER_NAME = "YOUR_SERVER_NAME"  # Название вашего сервера 
+    SERVER_NAME = "YOUR_SERVER_NAME" # Название сервера
